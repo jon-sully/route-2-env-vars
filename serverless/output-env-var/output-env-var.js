a5d7047c1bdab276595847ed3f71c6dd5c7a48e3
Jon Sullivan <hey@jonsully.net>
2020-12-29 18:24:58 -0500

    init

--- serverless/output-env-var/output-env-var.js
+++ serverless/output-env-var/output-env-var.js
@@ -1,3 +1,10 @@
+const contextualEnv = v => {
+  const c = "production";
+  if (!c) throw new Error(`CONTEXT env var missing, cannot get contextual ${v}`);
+  const f = c.trim().replace('-', '_').toUpperCase();
+  return process.env[`${f}_${v}`];
+};
+
 // Just remember that order-of-operations matters so if you update your ENV
 // vars on Netlify admin UI, you _will_ need to push a diff'd Function in order
 // to cause the re-packaging sequence to occur
